3975bc287a683da1698ebfb85c9843e65e3e8cc5
Couvelard Mathieu <couvelard.mathieu@gmail.com>
2018-07-24 11:35:08 +0200

    Ajout class Test

---
+++ TestDuSourceControl/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  TestDuSourceControl
+//
+//  Created by Couvelard Mathieu on 24/07/2018.
+//  Copyright © 2018 Couvelard Mathieu. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut() {
+        print("Salut les codabeilles")
+    }
+}
